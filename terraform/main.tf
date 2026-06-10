2bc7b8cc9774476cbc9b960bb4c85e30cbc2726e
Elendumir <makardmitr21@gmail.com>
2026-06-10 12:38:17 +0200

    Add  new roles

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,11 +16,19 @@ provider "aws" {
 resource "aws_security_group" "demo_sg" {
   name        = var.sg_name
   description = "Demo Security Group"
-
+  
   ingress {
     description = "SSH"
     from_port   = 22
     to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+  
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
